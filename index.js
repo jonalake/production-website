321fd21eacce6db6496c3e641c967d411043ce38
Jon Lake <jlake21@ford.com>
2022-04-11 15:34:27 -0600

    Fix Linting and Add User Input Validation

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const form = document.forms.input;
+const form = document.forms.input
 const stateDropdown = document.querySelector("select")
 
 function handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    const formData = new FormData(event.target);
-    const asString = new URLSearchParams(formData).toString();
+    const formData = new FormData(event.target)
+    const asString = new URLSearchParams(formData).toString()
     window.location.href = `results.html?${asString}`
 }
 
 fetch("./assets/states.json")
     .then(response => response.json())
-    .then(response => {
-        response.map(response => {
-            const state = document.createElement("option")
-            state.textContent = `${response.name}`
-            state.value = `${response.state_code}`
-            return state
-        }).forEach(state => {
-            stateDropdown.append(state)
+    .then(stateList => {
+        stateList.map(state => {
+            const option = document.createElement("option")
+            option.textContent = `${state.name}`
+            option.value = `${state.state_code}`
+            return option
+        }).forEach(option => {
+            stateDropdown.append(option)
         })
-
     })
 
-form.addEventListener('submit', handleSubmit)
+form.addEventListener("submit", handleSubmit)
--- results.js
+++ results.js
@@ -1,79 +1,112 @@
-const apiKey = 'b6c8d6cbabe24c6c8e6223207220604'
+const api = "b6c8d6cbabe24c6c8e6223207220604"
 const queryString = new URLSearchParams(window.location.search)
-const locations = `${queryString.get('location')} ${queryString.get('state')}`
+const loc = `${queryString.get("location")} ${queryString.get("state")}`
 const ul = document.querySelector("ul")
 const h1 = document.querySelector("h1")
 
-// fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`)
-fetch(`assets/demoWeather.json`)
+fetch(`https://api.weatherapi.com/v1/forecast.json?key=${api}&q=${loc}&days=3`)
+    // fetch("assets/demoWeather.json")
     .then(response => response.json())
     .then(weatherForecast => {
         console.log(weatherForecast)
         h1.textContent = weatherForecast.location.name
-        const current = weatherForecast.current
-        const day1 = weatherForecast.forecast.forecastday[0]
-        const today = document.createElement("li")
-        today.innerHTML = `
-            <h3>Today</h3>
-            <img src="${current.condition.icon}" alt="${current.condition.text}">
-            <h3>Current Temperature</h3>
-            <h2>${Math.round(current.temp_f)}°</h2>
-            <p>${current.condition.text}</p>
-            <p>High: ${Math.round(day1.day.maxtemp_f)}° | Low: ${Math.round(day1.day.mintemp_f)}°</p>
-        `
-        const details = document.createElement("div")
-        details.classList = "details hidden"
-        details.innerHTML = `
-            <p>Feels Like: ${Math.round(current.feelslike_f)}°</p>
-            <p>Wind: ${current.wind_mph} mph ${current.wind_dir}</p>
-            <p>Humidity: ${current.humidity}%</p>
-            <p>Visability: ${current.vis_miles} mi</p>
-            <p>Sunrise: ${day1.astro.sunrise}</p>
-            <p>Sunset: ${day1.astro.sunset}</p>
-            `
-        today.append(details)
-        let isClicked = true
-        today.addEventListener("click", event => {
-            if (isClicked) {
-                details.classList.remove("hidden")
-                isClicked = false
-            } else {
-                details.classList.add("hidden")
-                isClicked = true
-            }
-        })
-        ul.append(today)
+        ul.append(addCurrentDay(weatherForecast))
         const forecast = weatherForecast.forecast.forecastday
             .slice(1, weatherForecast.forecast.forecastday.length)
         forecast
             .map(date => {
-                const dayOfWeek = new Date(`${date.date.slice(0, 4)}`, `${date.date.slice(5, 7) - 1}`, `${date.date.slice(8, 10)}`).toLocaleDateString('en-US', { weekday: 'long', })
-                const day = document.createElement("li")
-                day.innerHTML = `
-                    <h3>${dayOfWeek}</h3>
-                    <img src="${date.day.condition.icon}" alt="${date.day.condition.text}">
-                    <h3>High</h3>
-                    <h2>${Math.round(date.day.maxtemp_f)}°</h2>
-                    <p>${date.day.condition.text}</p>
-                    <p>Low: ${Math.round(date.day.mintemp_f)}°</p>
-                `
-                const details = document.createElement("div")
-                details.classList = "details hidden"
-                details.innerHTML = `
-                    <p>Sunrise: ${date.astro.sunrise}</p>
-                    <p>Sunset: ${date.astro.sunset}</p>
-                    `
-                day.append(details)
-                let isClicked = true
-                day.addEventListener("click", event => {
-                    if (isClicked) {
-                        details.classList.remove("hidden")
-                        isClicked = false
-                    } else {
-                        details.classList.add("hidden")
-                        isClicked = true
-                    }
-                })
-                ul.append(day)
+                return ul.append(addForcastDay(date))
             })
     })
+    .catch(error => {
+        h1.textContent = "No matching location found."
+        console.error(error)
+    })
+
+function getDayofWeek(date) {
+    const year = date.slice(0, 4)
+    const month = date.slice(5, 7) - 1
+    const day = date.slice(8, 10)
+    return new Date(`${year}`, `${month}`, `${day}`)
+        .toLocaleDateString("en-US", { weekday: "long" })
+}
+
+function addCurrentDay(weatherForecast) {
+    const current = weatherForecast.current
+    const day1 = weatherForecast.forecast.forecastday[0]
+    const today = document.createElement("li")
+    today.innerHTML = `
+        <h3>Today</h3>
+        <img 
+            src="${current.condition.icon}" 
+            alt="${current.condition.text}"
+        >
+        <h3>Current Temperature</h3>
+        <h2>${Math.round(current.temp_f)}°</h2>
+        <p>${current.condition.text}</p>
+        <p>High: ${Math.round(day1.day.maxtemp_f)}° | Low: ${Math.round(day1.day.mintemp_f)}°</p>
+    `
+    const details = document.createElement("div")
+    details.classList = "details hidden"
+    details.innerHTML = `
+        <p>Feels Like: ${Math.round(current.feelslike_f)}°</p>
+        <p>Wind: ${current.wind_mph} mph ${current.wind_dir}</p>
+        <p>Humidity: ${current.humidity}%</p>
+        <p>Visability: ${current.vis_miles} mi</p>
+        <p>Sunrise: ${day1.astro.sunrise}</p>
+        <p>Sunset: ${day1.astro.sunset}</p>
+    `
+    today.append(details)
+    let isClicked = true
+    today.addEventListener("click", event => {
+        if (isClicked) {
+            details.classList.remove("hidden")
+            isClicked = false
+        } else {
+            details.classList.add("hidden")
+            isClicked = true
+        }
+    })
+    return today
+}
+
+const button = document.querySelector("button")
+button.addEventListener("click", handleSubmit)
+
+function addForcastDay(date) {
+    const dayOfWeek = getDayofWeek(date.date)
+    const day = document.createElement("li")
+    day.innerHTML = `
+        <h3>${dayOfWeek}</h3>
+        <img 
+            src="${date.day.condition.icon}" 
+            alt="${date.day.condition.text}"
+        >
+        <h3>High</h3>
+        <h2>${Math.round(date.day.maxtemp_f)}°</h2>
+        <p>${date.day.condition.text}</p>
+        <p>Low: ${Math.round(date.day.mintemp_f)}°</p>
+    `
+    const details = document.createElement("div")
+    details.classList = "details hidden"
+    details.innerHTML = `
+        <p>Sunrise: ${date.astro.sunrise}</p>
+        <p>Sunset: ${date.astro.sunset}</p>
+    `
+    day.append(details)
+    let isClicked = true
+    day.addEventListener("click", event => {
+        if (isClicked) {
+            details.classList.remove("hidden")
+            isClicked = false
+        } else {
+            details.classList.add("hidden")
+            isClicked = true
+        }
+    })
+    return day
+}
+
+function handleSubmit(event) {
+    window.location.href = "index.html"
+}
