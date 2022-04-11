72e3bd6ec436388544d1ab95658f5ee1bab42a64
Jon Lake <jlake21@ford.com>
2022-04-11 11:52:27 -0600

    Add Dynamic Content for Single Day and Styling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const form = document.forms.input;
+const stateDropdown = document.querySelector("select")
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -7,5 +8,19 @@ function handleSubmit(event) {
     const asString = new URLSearchParams(formData).toString();
     window.location.href = `results.html?${asString}`
 }
+
+fetch("./assets/states.json")
+    .then(response => response.json())
+    .then(response => {
+        response.map(response => {
+            const state = document.createElement("option")
+            state.textContent = `${response.name}`
+            state.value = `${response.state_code}`
+            return state
+        }).forEach(state => {
+            stateDropdown.append(state)
+        })
+
+    })
 
 form.addEventListener('submit', handleSubmit)
--- results.js
+++ results.js
@@ -1,9 +1,46 @@
-// const apiKey = 'NWM3ODI5NmMtZTFmZi00NmE3LTkwMDgtNGJkNjM3M2NiODgz'
-
+const apiKey = 'b6c8d6cbabe24c6c8e6223207220604'
 const queryString = new URLSearchParams(window.location.search)
-const searchParms = {
-    days: +`${queryString.get('forcast_length')}`,
-    location: `${queryString.get('location')}, ${queryString.get('state')}`,
-}
+const locations = `${queryString.get('location')} ${queryString.get('state')}`
+const ul = document.querySelector("ul")
+const h1 = document.querySelector("h1")
 
-console.log(searchParms)
+// fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`)
+fetch(`assets/demoWeather.json`)
+    .then(response => response.json())
+    .then(weatherForecast => {
+        console.log(weatherForecast)
+        h1.textContent = weatherForecast.location.name
+        const current = weatherForecast.current
+        const day1 = weatherForecast.forecast.forecastday[0]
+        const today = document.createElement("li")
+        today.innerHTML = `
+            <h3>Today</h3>
+            <img src="${current.condition.icon}" alt="${current.condition.text}">
+            <h3>Current Temperature</h3>
+            <h2>${Math.round(current.temp_f)}°</h2>
+            <p>${current.condition.text}</p>
+            <p>High: ${Math.round(day1.day.maxtemp_f)}° | Low: ${Math.round(day1.day.mintemp_f)}°</p>
+        `
+        const details = document.createElement("div")
+        details.classList = "details hidden"
+        details.innerHTML = `
+            <p>Feels Like: ${Math.round(current.feelslike_f)}°</p>
+            <p>Wind: ${current.wind_mph} mph ${current.wind_dir}</p>
+            <p>Humidity: ${current.humidity}%</p>
+            <p>Visability: ${current.vis_miles} mi</p>
+            <p>Sunrise: ${day1.astro.sunrise}</p>
+            <p>Sunset: ${day1.astro.sunset}</p>
+            `
+        today.append(details)
+        let isClicked = true
+        today.addEventListener("click", event => {
+            if (isClicked) {
+                details.classList.remove("hidden")
+                isClicked = false
+            } else {
+                details.classList.add("hidden")
+                isClicked = true
+            }
+        })
+        ul.append(today)
+    })
