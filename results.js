c60c16719fab5e98e5e8371a85a371173869b4b0
Jon Lake <jlake21@ford.com>
2022-04-13 10:12:37 -0600

    Add README.md and additional forcast details

--- results.js
+++ results.js
@@ -9,9 +9,8 @@ const button = document.querySelector("button")
 fetch(`https://api.weatherapi.com/v1/forecast.json?key=${api}&q=${loc}&days=3`)
     .then(response => response.json())
     .then(weatherForecast => {
-        const location = weatherForecast.location
-        h1.textContent = location.name
-        h4.textContent = `Displaying data for ${location.name}, ${location.region}, ${location.country}, as of ${location.localtime}`
+        console.log(weatherForecast)
+        setLocation(weatherForecast)
         ul.append(addCurrentDay(weatherForecast))
         const forecast = weatherForecast.forecast.forecastday
             .slice(1, weatherForecast.forecast.forecastday.length)
@@ -26,6 +25,17 @@ fetch(`https://api.weatherapi.com/v1/forecast.json?key=${api}&q=${loc}&days=3`)
         console.error(error)
     })
 
+button.addEventListener("click", handleSubmit)
+
+function setLocation(forecast) {
+    const location = forecast.location
+    h1.textContent = location.name
+    h4.textContent = `
+        Displaying data for ${location.name}, ${location.region}, 
+        ${location.country}, as of ${location.localtime}
+        `
+}
+
 function getDayofWeek(date) {
     const year = date.slice(0, 4)
     const month = date.slice(5, 7) - 1
@@ -36,18 +46,19 @@ function getDayofWeek(date) {
 
 function addCurrentDay(weatherForecast) {
     const current = weatherForecast.current
-    const day1 = weatherForecast.forecast.forecastday[0]
+    const day = weatherForecast.forecast.forecastday[0]
     const today = document.createElement("li")
     today.innerHTML = `
-        <h3>Today</h3>
+        <h2>Today</h2>
         <img 
             src="${current.condition.icon}" 
             alt="${current.condition.text}"
         >
-        <h3>Current Temperature</h3>
-        <h2>${Math.round(current.temp_f)}°</h2>
+        <h2>Current Temperature</h2>
+        <h3>${Math.round(current.temp_f)}°</h3>
         <p>${current.condition.text}</p>
-        <p>High: ${Math.round(day1.day.maxtemp_f)}° | Low: ${Math.round(day1.day.mintemp_f)}°</p>
+        <p>High: ${Math.round(day.day.maxtemp_f)}° | 
+        Low: ${Math.round(day.day.mintemp_f)}°</p>
     `
     const details = document.createElement("div")
     details.classList = "details hidden"
@@ -56,48 +67,54 @@ function addCurrentDay(weatherForecast) {
         <p>Wind: ${current.wind_mph} mph ${current.wind_dir}</p>
         <p>Humidity: ${current.humidity}%</p>
         <p>Visability: ${current.vis_miles} mi</p>
-        <p>Sunrise: ${day1.astro.sunrise}</p>
-        <p>Sunset: ${day1.astro.sunset}</p>
+        <p>Chance of Rain: ${day.day.daily_chance_of_rain}%</p>
+        <p>Chance of Snow: ${day.day.daily_chance_of_snow}%</p>
+        <p>Total Precipitation: ${day.day.totalprecip_in} in</p>
+        <p>Sunrise: ${day.astro.sunrise}</p>
+        <p>Sunset: ${day.astro.sunset}</p>
     `
     today.append(details)
-    let isClicked = true
-    today.addEventListener("click", event => {
-        if (isClicked) {
-            details.classList.remove("hidden")
-            isClicked = false
-        } else {
-            details.classList.add("hidden")
-            isClicked = true
-        }
-    })
+    showDetails(today, details)
     return today
 }
-
-button.addEventListener("click", handleSubmit)
 
 function addForcastDay(date) {
     const dayOfWeek = getDayofWeek(date.date)
     const day = document.createElement("li")
     day.innerHTML = `
-        <h3>${dayOfWeek}</h3>
+        <h2>${dayOfWeek}</h2>
         <img 
             src="${date.day.condition.icon}" 
             alt="${date.day.condition.text}"
         >
-        <h3>High</h3>
-        <h2>${Math.round(date.day.maxtemp_f)}°</h2>
+        <h2>High</h2>
+        <h3>${Math.round(date.day.maxtemp_f)}°</h3>
         <p>${date.day.condition.text}</p>
         <p>Low: ${Math.round(date.day.mintemp_f)}°</p>
     `
     const details = document.createElement("div")
     details.classList = "details hidden"
     details.innerHTML = `
+        <p>Average Humidity: ${date.day.avghumidity}%</p>
+        <p>Average Visability: ${date.day.avgvis_miles} mi</p>
+        <p>Chance of Rain: ${date.day.daily_chance_of_rain}%</p>
+        <p>Chance of Snow: ${date.day.daily_chance_of_snow}%</p>
+        <p>Total Precipitation: ${date.day.totalprecip_in} in</p>
         <p>Sunrise: ${date.astro.sunrise}</p>
         <p>Sunset: ${date.astro.sunset}</p>
     `
     day.append(details)
+    showDetails(day, details)
+    return day
+}
+
+function handleSubmit() {
+    window.location.href = "index.html"
+}
+
+function showDetails(day, details) {
     let isClicked = true
-    day.addEventListener("click", event => {
+    day.addEventListener("click", () => {
         if (isClicked) {
             details.classList.remove("hidden")
             isClicked = false
@@ -107,8 +124,4 @@ function addForcastDay(date) {
         }
     })
     return day
-}
-
-function handleSubmit(event) {
-    window.location.href = "index.html"
 }
